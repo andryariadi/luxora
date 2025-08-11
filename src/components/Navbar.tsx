c2d4a37294617ff5fadfa43adccf02696af382f5
Andry Ariadi <andryariadi23@gmail.com>
2025-08-11 13:45:02 +0800

    feat(footer): add Footer component with styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,9 @@ import ShoppingCartIcon from "./ShoppingCartIcon";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white container flex items-center justify-between border-b border-gray-200 py-2">
+    <nav className="bg-white container flex items-center justify-between border-b border-gray-200 py-3">
       {/* Left */}
-      <Link href="/" className="flex items-center">
+      <Link href="/" className="flex items-center hover:scale-105 transition-all duration-300">
         <Image src="/logo.png" alt="Luxora" width={36} height={36} className="w-6 h-6 md:w-9 md:h-9" />
 
         <p className="hidden md:block text-dark-200 text-md font-medium tracking-wider uppercase">Luxora.</p>
---
+++ src/libs/constant/index.ts
@@ -0,0 +1,20 @@
+export const footerLinks = [
+  { href: "/", label: "Home" },
+  { href: "/contact", label: "Contact" },
+  { href: "/terms-of-service", label: "Terms of Service" },
+  { href: "/privacy-policy", label: "Privacy Policy" },
+];
+
+export const footerLinksProduct = [
+  { href: "/products", label: "All Products" },
+  { href: "/new-arrivals", label: "New Arrivals" },
+  { href: "/best-sellers", label: "Best Sellers" },
+  { href: "/sales", label: "Sales" },
+];
+
+export const footerLinksCompany = [
+  { href: "/about", label: "About" },
+  { href: "/contact", label: "Contact" },
+  { href: "/blog", label: "Blog" },
+  { href: "/affiliate-program", label: "Affiliate Program" },
+];
