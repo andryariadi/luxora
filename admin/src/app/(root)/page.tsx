d07a72f9be9578a9489b3ab32c4df6eebf129450
Andry Ariadi <andryariadi23@gmail.com>
2025-08-21 07:24:45 +0800

    feat(TableUsers): add TableUser component

--- admin/src/app/(root)/page.tsx
+++ admin/src/app/(root)/page.tsx
@@ -7,7 +7,7 @@ import TotalVisitorAreaChart from "@/components/TotalVisitorAreaChart";
 export default function HomePage() {
   return (
     <div
-      className="b-rose-500 p-4
+      className="b-rose-500 page
      grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4"
     >
       <div className="bg-primary-foreground shadow-sm dark:shadow-none p-4 rounded-lg border border-sidebar-border lg:col-span-2 xl:col-span-1 2xl:col-span-2">
